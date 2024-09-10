1c07aac7cd1e2f9c239a47f9a011d379286619aa
Franco Moloche <francomolocheg99@gmail.com>
2024-09-09 23:31:41 -0500

    Refactor interactive-retro-terrain.js

--- src/scenes/interactive-retro-terrain.js
+++ src/scenes/interactive-retro-terrain.js
@@ -1,4 +1,4 @@
-let tina, resizeGraphics
+let tina
 
 const player = {
   pos: [-1, -1],
@@ -10,112 +10,110 @@ const player = {
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
-  resizeGraphics = () => {
-    tina = new Tina(160, round(160 * (height / width)))
-    tina.build(/* glsl */ `
-      uniform vec2 playerP;
-      uniform float playerAngle;
-      const float minBri = 0.25;
+  tina = new Tina(160, 160 * (height / width))
 
-      struct PLight {
-        vec3 pos;
-        vec3 color;
-        float r;
-        float dist;
-      } pl;
+  tina.build(/* glsl */ `
+    uniform vec2 playerP;
+    uniform float playerAngle;
+    const float minBri = 0.25;
 
-      float sdTerrain(vec3 p) {
-        float height = snoise(vec3(p.xz, p.y - time / 10.));
-        return p.y - height;
-      }
+    struct PLight {
+      vec3 pos;
+      vec3 color;
+      float r;
+      float dist;
+    } pl;
 
-      float sdScene(vec3 p) {
-        pl.dist = sdSphere(p - pl.pos, pl.r);
-        float terrainD = sdTerrain(p);
-        return min(terrainD, pl.dist);
-      }
+    float sdTerrain(vec3 p) {
+      float height = snoise(vec3(p.xz, p.y - time / 10.));
+      return p.y - height;
+    }
 
-      vec3 calcNormal(vec3 p) {
-        const float eps = 1e-3;
-        vec3 h = vec3(eps, 0.0, 0.0);
-        return normalize(vec3(
-          sdScene(p + h.xyy) - sdScene(p - h.xyy),
-          sdScene(p + h.yxy) - sdScene(p - h.yxy),
-          sdScene(p + h.yyx) - sdScene(p - h.yyx)
-        ));
-      }
+    float sdScene(vec3 p) {
+      pl.dist = sdSphere(p - pl.pos, pl.r);
+      float terrainD = sdTerrain(p);
+      return min(terrainD, pl.dist);
+    }
 
-      vec3 applyPointLight(vec3 p, vec3 normal, vec3 lightPos, vec3 lightColor, vec3 baseColor) {
-        vec3 lightDir = normalize(pl.pos - p);
-        float diff = max(dot(normal, lightDir), minBri);
-        return lightColor * baseColor * diff;
-      }
+    vec3 calcNormal(vec3 p) {
+      const float eps = 1e-3;
+      vec3 h = vec3(eps, 0.0, 0.0);
+      return normalize(vec3(
+        sdScene(p + h.xyy) - sdScene(p - h.xyy),
+        sdScene(p + h.yxy) - sdScene(p - h.yxy),
+        sdScene(p + h.yyx) - sdScene(p - h.yyx)
+      ));
+    }
 
-      vec3 calcPos(vec3 ro, vec3 rd) {
-        float z = 0.;
-        for(int i = 0; i < 1024; i++) {
-          vec3 p = ro + z * rd;
-          float d = sdScene(p) / 8.;
-          if(d < 1e-3) {
-            return p;
-          }
-          z += d;
-          if(z > 1e3) break;
+    vec3 applyPointLight(vec3 p, vec3 normal, vec3 lightPos, vec3 lightColor, vec3 baseColor) {
+      vec3 lightDir = normalize(pl.pos - p);
+      float diff = max(dot(normal, lightDir), minBri);
+      return lightColor * baseColor * diff;
+    }
+
+    vec3 calcPos(vec3 ro, vec3 rd) {
+      float z = 0.;
+      for(int i = 0; i < 1024; i++) {
+        vec3 p = ro + z * rd;
+        float d = sdScene(p) / 8.;
+        if(d < 1e-3) {
+          return p;
         }
-        return vec3(0.);
+        z += d;
+        if(z > 1e3) break;
       }
+      return vec3(0.);
+    }
 
-      ---
+    ---
 
-      uv = uv * 2. - 1.;
-      uv.x *= width/height;
-      vec3 ro = vec3(0.);
-      vec3 rd = vec3(uv, -1.);
-      ro *= rotateY(playerAngle);
-      ro.xz += playerP;
-      ro.y += .8;
-      rd *= rotateY(playerAngle);
+    uv = uv * 2. - 1.;
+    uv.x *= width/height;
+    vec3 ro = vec3(0.);
+    vec3 rd = vec3(uv, -1.);
+    ro *= rotateY(playerAngle);
+    ro.xz += playerP;
+    ro.y += .8;
+    rd *= rotateY(playerAngle);
 
-      pl = PLight(
-        vec3(0., 100., -250.),
-        vec3(.9, 1., 1.),
-        60.,
-        0.
-      );
+    pl = PLight(
+      vec3(0., 100., -250.),
+      vec3(.9, 1., 1.),
+      60.,
+      0.
+    );
 
-      vec3 p = calcPos(ro, rd);
+    vec3 p = calcPos(ro, rd);
 
-      if(pl.dist < 0.01) {
-        fragColor = vec4(pl.color, 1.);
-        return;
-      }
+    if(pl.dist < 0.01) {
+      fragColor = vec4(pl.color, 1.);
+      return;
+    }
 
-      if(length(p) <= 1e-4) {
-        fragColor = vec4(vec3(0.), 1.);
-        return;
-      }
+    if(length(p) <= 1e-4) {
+      fragColor = vec4(vec3(0.), 1.);
+      return;
+    }
 
-      vec3 lightDir = normalize(p - pl.pos);
-      vec3 lightRayP = calcPos(
-        pl.pos + lightDir * (pl.r + 0.01), lightDir
-      );
+    vec3 lightDir = normalize(p - pl.pos);
+    vec3 lightRayP = calcPos(
+      pl.pos + lightDir * (pl.r + 0.01), lightDir
+    );
 
-      float obstacleDist = length(lightRayP - p);
-      float visibility = 1.;
-      if(obstacleDist > .1) {
-        visibility = 0.25;
-      }
+    float obstacleDist = length(lightRayP - p);
+    float visibility = 1.;
+    if(obstacleDist > .1) {
+      visibility = 0.25;
+    }
 
-      vec3 normal = calcNormal(p);
-      vec3 lighting = applyPointLight(
-        p, normal, pl.pos, pl.color, vec3(1.)
-      ) * visibility;
+    vec3 normal = calcNormal(p);
+    vec3 lighting = applyPointLight(
+      p, normal, pl.pos, pl.color, vec3(1.)
+    ) * visibility;
 
-      fragColor = vec4(lighting, 1.);
-    `)
-  }
+    fragColor = vec4(lighting, 1.);
+  `)
 
-  resizeGraphics()
   noSmooth()
 }
 
@@ -133,7 +131,7 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
-  resizeGraphics()
+  tina.resize(160, 160 * (height / width))
 }
 
 function mousePressed() {
