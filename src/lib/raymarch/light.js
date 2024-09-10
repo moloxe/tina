3999261d7ed3855e63b469affe68baa54b4737f5
Franco Moloche <francomolocheg99@gmail.com>
2024-09-10 12:54:05 -0500

    fix: viewDir calc for poin light

--- src/lib/raymarch/light.js
+++ src/lib/raymarch/light.js
@@ -12,17 +12,17 @@ uniform PointLight pointLights[${POINT_LIGHTS}];
 const vec3 specColor = vec3(1.); // For now, white is fine
 vec3 applyPointLight(
   vec3 pos, vec3 diffuseColor, float shininess,
-  vec3 normal, vec3 ambientColor, PointLight pointLight
+  vec3 normal, vec3 ambientColor, PointLight pointLight, vec3 viewDir
 ) {
   vec3 lightDir = pointLight.pos - pos;
   float distance = length(lightDir);
-  lightDir = normalize(lightDir);
   distance *= distance;
+  lightDir = normalize(lightDir);
+
   float lambertian = max(dot(normal, lightDir), 0.);
   float specular = 0.;
 
   if (lambertian > 0.) {
-    vec3 viewDir = normalize(pos);
     vec3 halfDir = normalize(lightDir + viewDir);
     float specAngle = max(dot(halfDir, normal), 0.);
     specular = pow(specAngle, shininess);
@@ -48,6 +48,7 @@ vec3 calcLighting(vec3 ro, vec3 rd) {
   }
 
   vec3 pos = rm.pos;
+  vec3 viewDir = -rd;
   Material material = materials[rm.materialIndex];
 
   vec3 normal = calcSceneNormal(pos);
@@ -68,7 +69,7 @@ vec3 calcLighting(vec3 ro, vec3 rd) {
           lighting,
           visibility * applyPointLight(
             pos, diffuseColor, shininess,
-            normal, ambientColor, pointLights[${i}]
+            normal, ambientColor, pointLights[${i}], viewDir
           )
         );
       `
