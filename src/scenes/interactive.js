4b850eb24e36409df8af9684cda73a742cdbb105
Franco Moloche <francomolocheg99@gmail.com>
2024-09-12 21:38:26 -0500

    Add capsule physics to example

--- src/scenes/interactive.js
+++ src/scenes/interactive.js
@@ -1,17 +1,23 @@
-function Player({ pos, cam }) {
-  this.pos = pos
+function Player() {
+  const initialPos = [0, 1, 1]
   this.vel = [0, 0, 0]
-  this.cam = cam // spherical coordinates
+  this.cam = [0.6, 0, -0.5] // spherical coordinates
   this.onTheFloor = true
+  this.body = new CapsulePhysics({
+    pos: [...initialPos],
+    end: [0, -0.1, 0],
+    radius: 0.05,
+  })
+  this.material = null
 
-  this.maxVel = 0.1
-  this.acc = 0.002
+  this.maxVel = 0.02
+  this.acc = 0.003
 
   this.move = (move) => {
     const yAngle = this.cam[1]
 
     if (move === 'UP' && this.onTheFloor) {
-      this.vel[1] += 0.05
+      this.vel[1] += 0.06
       this.onTheFloor = false
     }
     if (move === 'DOWN') {
@@ -36,49 +42,74 @@ function Player({ pos, cam }) {
       this.vel[0] += this.acc * cos(yAngle) * slowDown
       this.vel[2] -= this.acc * sin(yAngle) * slowDown
     }
-
-    const xzVel = sqrt(this.vel[0] ** 2 + this.vel[2] ** 2)
-    if (xzVel > this.maxVel) {
-      this.vel[0] *= this.maxVel / xzVel
-      this.vel[2] *= this.maxVel / xzVel
-    }
   }
 
-  // TODO: Add collision detection using raymarching
-  this.startPhysics = () => {
-    const freq = 1000 / 60
-    const gravity = 0.02
-    setInterval(() => {
-      this.pos[0] += this.vel[0]
-      this.vel[0] *= 0.95
-      this.pos[2] += this.vel[2]
-      this.vel[2] *= 0.95
-      this.pos[1] += this.vel[1]
-      this.vel[1] -= gravity / freq
-      if (this.pos[1] < 0) {
-        this.onTheFloor = true
-        this.vel[1] = 0
-        this.pos[1] = 0
-      }
-    }, freq)
+  // TODO: delta time has to be considered
+  this.updatePhysics = () => {
+    const prevPos = [...this.body.pos]
+    this.body.pos[0] += this.vel[0]
+    this.body.pos[1] += this.vel[1]
+    this.body.pos[2] += this.vel[2]
+    const collisions = this.body.getCollisionMap()
+    let collideX = false
+    let collideY = false
+    let collideZ = false
+    collisions.loadPixels()
+    for (let i = 0; i < collisions.pixels.length; i += 4) {
+      const normalX = (collisions.pixels[i] / 255) * 2 - 1
+      const normalY = (collisions.pixels[i + 1] / 255) * 2 - 1
+      const normalZ = (collisions.pixels[i + 2] / 255) * 2 - 1
+      const module = sqrt(normalX ** 2 + normalY ** 2 + normalZ ** 2)
+      if (module < 0.1) continue
+      // TODO: add direction to the collisions
+      if (abs(normalX) > 0.1) collideX = true
+      if (abs(normalY) > 0.1) collideY = true
+      if (abs(normalZ) > 0.1) collideZ = true
+    }
+    if (collideX) {
+      this.body.pos[0] = prevPos[0]
+      this.vel[0] = 0
+    }
+    if (collideY) {
+      this.body.pos[1] = prevPos[1]
+      this.vel[1] = -this.vel[1] * 0.2 // bounce
+      this.onTheFloor = true
+    }
+    if (collideZ) {
+      this.body.pos[2] = prevPos[2]
+      this.vel[2] = 0
+    }
+    this.vel[0] *= 0.9
+    this.vel[1] -= 0.002 // gravity
+    this.vel[2] *= 0.9
+
+    if (this.body.pos[1] < -5) this.body.pos = [...initialPos] // fall
+
+    this.material.pos = this.body.pos
+
+    return collisions
   }
 }
 
-const player = new Player({
-  pos: [0, 1, 1],
-  cam: [0.6, 0, -0.5],
-})
-
-let tina,
+let player,
+  tina,
   fps,
   lights = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
-  tina = new Tina(width, height)
+  tina = new Tina(...getResolution())
+
+  player = new Player()
 
   tina.setScene((scene) => {
+    player.material = scene.capsule({
+      shininess: 512,
+      radius: player.body.radius * 1,
+      end: player.body.end,
+    })
+
     scene.sphere({
       shininess: 512,
       color: [1, 1, 1],
@@ -86,7 +117,18 @@ function setup() {
     })
 
     scene.box({
-      shininess: 512,
+      rotation: [-0.6, 0, 0],
+      pos: [1, 0, 0],
+      dimensions: [0.2, 0.01, 0.3],
+    })
+
+    scene.box({
+      rotation: [0, 0, 0],
+      pos: [1, 0.168, -0.54],
+      dimensions: [0.2, 0.01, 0.3],
+    })
+
+    scene.box({
       pos: [0, 0.05, 0],
       dimensions: [0.3, 0.05, 0.3],
     })
@@ -106,12 +148,6 @@ function setup() {
       color: [0.9, 0.9, 0.6],
     })
 
-    playerMaterial = scene.sphere({
-      shininess: 512,
-      color: [1, 1, 1],
-      radius: 0.1,
-    })
-
     lights[0] = scene.pointLight({
       color: [1, 0, 0],
       power: 3,
@@ -127,6 +163,8 @@ function setup() {
       power: 3,
     })
   })
+
+  player.body.build(tina.scene)
 
   tina.build(/* glsl */ `
     struct Player {
@@ -157,16 +195,13 @@ function setup() {
 
   controlsListener()
   noSmooth()
-  player.startPhysics()
 
   setInterval(() => {
     fps = frameRate().toFixed(0)
-  }, 500)
+  }, 300)
 }
 
 function draw() {
-  playerMaterial.pos = player.pos
-
   for (let i = 0; i < lights.length; i++) {
     lights[i].pos = [
       cos(frameCount / 200 + (i * TWO_PI) / lights.length) * 3,
@@ -176,11 +211,14 @@ function draw() {
   }
 
   const graphics = tina.update({
-    ['player.pos']: player.pos,
+    ['player.pos']: player.body.pos,
     ['player.cam']: player.cam,
   })
 
+  const collisions = player.updatePhysics()
+
   image(graphics, 0, 0, width, height)
+  image(collisions, 0, height / 2, width / 6, width / 6)
 
   fill('#00ff00')
   noStroke()
@@ -188,6 +226,7 @@ function draw() {
   textSize(16)
   stroke('#000')
   text(`FPS: ${fps}`, 10, 30)
+  text(`Collision map`, 5, height / 2 + 20)
 }
 
 function controlsListener() {
@@ -216,12 +255,16 @@ function mouseMoved() {
   player.cam[2] -= movedY / 300
 }
 
+function getResolution() {
+  return [256 * (width / height), 256]
+}
+
 let resizeTimeout
 function windowResized() {
   if (resizeTimeout) clearTimeout(resizeTimeout)
   resizeTimeout = setTimeout(() => {
     resizeCanvas(windowWidth, windowHeight)
-    tina.resize(width, height)
+    tina.resize(...getResolution())
     resizeTimeout = null
   }, 100)
 }
