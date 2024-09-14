f8cd74ff311efb5cb561070c5f71969a12731dea
Franco Moloche <francomolocheg99@gmail.com>
2024-09-14 13:10:42 -0500

    Updating landing

--- src/components/TinaExamples.tsx
+++ src/components/TinaExamples.tsx
@@ -12,7 +12,7 @@ type Props = {
 }
 
 const TinaExamples: FC<Props> = ({ examples }) => {
-  const [currentExample, setCurrentExample] = useState(4)
+  const [currentExample, setCurrentExample] = useState(3)
   const example = examples[currentExample]
 
   const sketch = useMemo(() => {
@@ -26,18 +26,32 @@ const TinaExamples: FC<Props> = ({ examples }) => {
   return (
     <>
       <ul>
-        {examples.map((example, index) => (
-          <li key={example.route}>
-            <button
-              onClick={() => setCurrentExample(index)}
-              className={`underline ${
-                index === currentExample ? 'text-primary' : ''
-              }`}
-            >
-              {example.route}
-            </button>
-          </li>
-        ))}
+        {examples.map((example, index) => {
+          const isCurrent = index === currentExample
+          return (
+            <li key={example.route}>
+              {isCurrent ? (
+                <>
+                  <a
+                    href={`/tina/${example.route}`}
+                    target="_blank"
+                    className="underline text-primary"
+                  >
+                    {example.route}
+                  </a>
+                  {' — Click again to open in new tab'}
+                </>
+              ) : (
+                <button
+                  onClick={() => setCurrentExample(index)}
+                  className="underline"
+                >
+                  {example.route}
+                </button>
+              )}
+            </li>
+          )
+        })}
       </ul>
 
       <iframe src={`/tina/${example.route}`} height="400" width="100%" />
